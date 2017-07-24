978d8642fdf647cc972e51f5f68c5baf3d60c467
GLOBAL\I336488 <mihaela.costea2010@gmail.com>
2017-07-24 11:22:12 +0300

    hello world with nodejs server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,28 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+
+// variables
+var iPort = 8081;
+
+//required module
+var mHttp = require("http");
+
+//create Http server
+var oServer = mHttp.createServer(function(req, res){
+
+    console.log("request received.");
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end();
+
+    console.log("Response sent !");
+
+});
+oServer.listen(iPort);
+
+//Console will print the server info
+console.log("Server running at http://127.0.0.1:"+ iPort + "/");
+
+// var sMessage = "Hello world!";
+// sMessage = sMessage + "!";
+
+// console.log(sMessage);
